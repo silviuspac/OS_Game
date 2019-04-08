fefee1038b1c978414bab606dfd9197bb25bf08a
silviuspac <silviu.spac@outlook.com>
2019-04-08 20:08:11 +0200

    gestione errori

--- common.h
+++ common.h
@@ -1,2 +1,19 @@
 #ifndef COMMON_H
 #define COMMON_H
+
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+// Error handling
+#define GENERIC_ERROR_HELPER(cond, errCode, msg) do {               \
+        if (cond) {                                                 \
+            fprintf(stderr, "%s: %s\n", msg, strerror(errCode));    \
+            exit(EXIT_FAILURE);                                     \
+        }                                                           \
+    } while(0)
+
+#define ERROR_HELPER(ret, msg)          GENERIC_ERROR_HELPER((ret < 0), errno, msg)
+#define PTHREAD_ERROR_HELPER(ret, msg)  GENERIC_ERROR_HELPER((ret != 0), ret, msg)
